e0e3238c25b29a190e22be95d75d66d00ed44a50
sunu-nandakumar <appu1998.nandakumar@gmail.com>
2021-06-30 16:18:15 +0530

    adding the 4th uc

--- Main.java
+++ Main.java
@@ -5,12 +5,12 @@ public class Main {
 	public static void main(String[] args) {
 
 		LinkedList linkedList = new LinkedList();
-		linkedList.push(56);
+		LinkedList.Node previousNode = linkedList.insert(56);
 
-		linkedList.insert(30);
-		linkedList.insert(70);
+		LinkedList.Node afterNode = linkedList.insert(70);
+		linkedList.insertBefore(previousNode, afterNode, 30);
 
-		linkedList.print();
+		
 	}
 
 }
